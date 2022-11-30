006b22db86f12186eaefc6fa72ababcf7da4f626
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-29 20:51:01 -0700

    Edited link for states on list of states page

--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe 'The site shows a list of states' do
   it 'displays states names' do
     washington = State.create!(name:"Washington", population:7.739, gdp:577.2, size:71362, coastal:true)
     colorado = State.create!(name:"Colorado", population:5.812, gdp:373.76, size:104185, coastal:true)
-    binding.pry
     visit "/states/#{washington.id}"
 
     expect(page).to have_content(washington.title)
