29161de385cac58e6929c41ae5468a2b9ef79bcb
Leo Banos Garcia <bsleo625@gmail.com>
2022-12-01 16:08:06 -0700

    Test: add before each to city spec and a state-city ex

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,21 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  it 'city has attributes' do
-    seattle = City.create!(name: "Seattle", 
-        population: 733919, 
-        owner_occupied_housing_unit_rate: 44.9, 
-        form_of_gov: "Mayor-Council", 
-        size: 83.78, 
-        median_household_income: 97185, 
-        public_transit: true)
+  before :each do
+    @washington = State.create!(name: "Washington", 
+                                population: 7.739, 
+                                gdp: 577.2, 
+                                size: 71362, 
+                                coastal: true)
 
-    expect(seattle.name).to eq("Seattle")
-    expect(seattle.population).to eq(733919)
-    expect(seattle.owner_occupied_housing_unit_rate).to eq(44.9)
-    expect(seattle.form_of_gov).to eq("Mayor-Council")
-    expect(seattle.size).to eq(83.78)
-    expect(seattle.median_household_income).to eq(97185)
-    expect(seattle.public_transit).to be(true)
+    @seattle = @washington.cities.create!(name: "Seattle", 
+    population: 733919, 
+    owner_occupied_housing_unit_rate: 44.9, 
+    form_of_gov: "Mayor-Council", 
+    size: 83.78, 
+    median_household_income: 97185, 
+    public_transit: true,
+    state_id: @washington.id)
+  end
+
+  it 'city has attributes' do
+
+    expect(@seattle.name).to eq("Seattle")
+    expect(@seattle.population).to eq(733919)
+    expect(@seattle.owner_occupied_housing_unit_rate).to eq(44.9)
+    expect(@seattle.form_of_gov).to eq("Mayor-Council")
+    expect(@seattle.size).to eq(83.78)
+    expect(@seattle.median_household_income).to eq(97185)
+    expect(@seattle.public_transit).to be(true)
+
+  end
+
+  describe "#state" do
+    xit "returns the associated state" do
+
+    end
   end
 end
