07a96e0dd93375eeacfd86273fd65d30536762d0
Leo Banos Garcia <bsleo625@gmail.com>
2022-12-01 15:50:51 -0700

    Add spec file for state_city

---
+++ spec/features/states/cities/state_cities_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe "State's City Index", type: :feature do
+  describe "When I visit states/:state_id/city" do
+    @washington = State.create!(name:"Washington", 
+        population:7.739, 
+        gdp:577.2, 
+        size:71362, 
+        coastal:true)
+      @seattle = @washington.cities.create!(name: "Seattle", 
+        population: 733919, 
+        owner_occupied_housing_unit_rate: 44.9, 
+        form_of_gov: "Mayor-Council", 
+        size: 83.78, 
+        median_household_income: 97185, 
+        public_transit: true)
+      @spokane = @washington.cities.create!(name: "Spokane", 
+        population: 229071, 
+        owner_occupied_housing_unit_rate: 56.0, 
+        form_of_gov: "Mayor-Council", 
+        size: 69.49, 
+        median_household_income: 52600, 
+        public_transit: true)
+      @colorado = State.create!(name:"Colorado", 
+        population:5.812, 
+        gdp:373.76, 
+        size:104185, 
+        coastal:false)
+      @denver = @colorado.cities.create!(name: "Denver", 
+          population: 711463, 
+          owner_occupied_housing_unit_rate: 50.4, 
+          form_of_gov: "Consolidated City-County", 
+          size: 154.7, 
+          median_household_income: 72661, 
+          public_transit: true)
+    it "See each city that is associated with the state" do
+        visit "/states/#{washington.id}/cities"
+        save_and_open_page
+
+        expect(page).to have_content(@seattle.name)
+        expect(page).to have_content(@spokane.name)
+        expect(page).to_not have_content(@denver.name)
+    end
+
+    it "See each city's attributes"
+        
+        # expect(page).to have_content("Population Estimates, July 1, 2021: #{@seattle.population}")
+        # expect(page).to have_content("Owner Occupied Housing Unit Rate, 2016-2020: #{@seattle.owner_occupied_housing_unit_rate}%")
+        # expect(page).to have_content("Population Estimates, July 1, 2021: #{@spokane.population}")
+        # expect(page).to have_content("Owner Occupied Housing Unit Rate, 2016-2020: #{@spokane.owner_occupied_housing_unit_rate}%")
+        # expect(page).to have_content("Form of Government: #{@spokane.form_of_gov}")
+        # expect(page).to_not have_content(@denver.population)
+  end
+end
