4f37862f9afd8d379cc7ef2eae0635fdb1c5acd0
Leo Banos Garcia <bsleo625@gmail.com>
2022-12-04 14:54:19 -0700

    Add post route to states#create and #create method

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -8,6 +8,8 @@ class StatesController < ApplicationController
   end
 
   def new
+  end
 
+  def create
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'welcome#index'
+
   get '/states', to: 'states#index'
   get 'states/new', to: 'states#new'
+  post '/states', to: 'states#create'
   get '/states/:id', to: 'states#show'
+
   get '/cities', to: 'cities#index'
   get '/cities/:id', to: 'cities#show'
   get '/states/:state_id/cities', to: 'state_cities#index'
