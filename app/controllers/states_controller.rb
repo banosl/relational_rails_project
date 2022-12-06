e3fa1705370f5c2f00687be217befca13a2afc16
Leo Banos Garcia <bsleo625@gmail.com>
2022-12-06 15:52:40 -0700

    Refactor changed the edit state form to use rails view helpers

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -41,6 +41,6 @@ class StatesController < ApplicationController
 
 private
   def state_params
-    params.require(:State).permit(:name, :population, :gdp, :size, :coastal)
+    params.permit(:name, :population, :gdp, :size, :coastal)
   end
 end
